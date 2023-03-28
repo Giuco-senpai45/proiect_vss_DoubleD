37b07eccd36ff367652da339fb7223b0477a2cb1
Denisa Ateodoresi <denisa.ateodoresi@stud.ubbcluj.ro>
2023-03-28 14:27:50 +0300

    Added more annotations and generated report from testlink

--- src/test/inventory/service/InventoryServiceTest.java
+++ src/test/inventory/service/InventoryServiceTest.java
@@ -1,10 +1,7 @@
 package inventory.service;
 
 import inventory.repository.InventoryRepository;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.io.BufferedWriter;
 import java.io.File;
@@ -13,7 +10,7 @@ import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.fail;
 
-class InventoryServiceTest {
+public class InventoryServiceTest {
 
     private static final int STOC = 5;
     private static final int MIN = 2;
@@ -31,8 +28,19 @@ class InventoryServiceTest {
         service = new InventoryService(repo);
     }
 
-    @Test
+    @AfterAll
+    static void tearDown() throws IOException {
+        File file = new File("data/items_test.txt");
+        BufferedWriter bw = new BufferedWriter(new FileWriter(file));
+        bw.write("");
+    }
+
+    @RepeatedTest(3)
     @DisplayName("TC1_ECP: Add new inhouse part successfully")
+    @Tag("ECP")
+    @Tag("inhousePart")
+    @Tag("price")
+    @Tag("name")
     void addInhousePartValid_TC1_ECP() {
         // Arrange
         String validName = "InhousePart";
@@ -45,7 +53,9 @@ class InventoryServiceTest {
 
     @Test
     @DisplayName("TC2_ECP: Add new inhouse part with empty name")
-    @Tag("invalid_name")
+    @Tag("ECP")
+    @Tag("inhousePart")
+    @Tag("name")
     void addInhousePartInvalid_TC2_ECP() {
         String invalidName = "";
         double validPrice = 23.05;
@@ -59,7 +69,9 @@ class InventoryServiceTest {
 
     @Test
     @DisplayName("TC3_ECP: Add new inhouse part with price 0")
-    @Tag("invalid_price")
+    @Tag("ECP")
+    @Tag("inhousePart")
+    @Tag("price")
     void addInhousePartInvalid_TC3_ECP() {
         String validName = "InhousePart";
         double invaldPrice = 0;
@@ -73,6 +85,9 @@ class InventoryServiceTest {
 
     @Test
     @DisplayName("TC3_BVA: Add new inhouse part successfully")
+    @Tag("BVA")
+    @Tag("inhousePart")
+    @Tag("name")
     void addInhousePartValid_TC3_BVA() {
         String validName = "P";
         double validPrice = 23.05;
@@ -82,6 +97,9 @@ class InventoryServiceTest {
 
     @Test
     @DisplayName("TC4_BVA: Add new inhouse part successfully")
+    @Tag("BVA")
+    @Tag("inhousePart")
+    @Tag("name")
     void addInhousePartValid_TC4_BVA() {
         String validName = "P...................................................................................." +
                 "..................................................................................." +
@@ -93,6 +111,9 @@ class InventoryServiceTest {
 
     @Test
     @DisplayName("TC5_BVA: Add new inhouse part successfully")
+    @Tag("BVA")
+    @Tag("inhousePart")
+    @Tag("name")
     void addInhousePartValid_TC5_BVA() {
         String validName = "P...................................................................................." +
                 "..................................................................................." +
@@ -104,6 +125,9 @@ class InventoryServiceTest {
 
     @Test
     @DisplayName("TC8_BVA: Add new inhouse part successfully")
+    @Tag("BVA")
+    @Tag("inhousePart")
+    @Tag("price")
     void addInhousePartValid_TC8_BVA() {
         String validName = "P";
         double validPrice = 0.01;
@@ -113,7 +137,9 @@ class InventoryServiceTest {
 
     @Test
     @DisplayName("TC9_BVA: Add new inhouse part with price less than 0")
-    @Tag("invalid_price")
+    @Tag("BVA")
+    @Tag("inhousePart")
+    @Tag("price")
     void addInhousePartInvalid_TC9_BVA() {
         String validName = "P";
         double invalidPrice = -0.01;
@@ -122,6 +148,50 @@ class InventoryServiceTest {
             fail();
         } catch (RuntimeException e) {
             assert e.getMessage().equals("The price must be greater than 0. ");
+        }
+    }
+
+    @Nested
+    @DisplayName("BVA tests for add inhouse part with focus on price upper limit")
+    class BVAPriceUpperLimit {
+        @Test
+        @Tag("BVA")
+        @Tag("inhousePart")
+        @Tag("price")
+        void addInhousePartValid_TC10_BVA() {
+            String validName = "P";
+            double validPrice = Double.MAX_VALUE;
+            service.addInhousePart(validName, validPrice, STOC, MIN, MAX, MASINA_ID);
+            assert service.getAllParts().size() == 1;
+        }
+
+        @Test
+        @Tag("BVA")
+        @Tag("inhousePart")
+        @Tag("price")
+        void addInhousePartValid_TC11_BVA() {
+            String validName = "P";
+            double validPrice = Double.MAX_VALUE - 0.01;
+            service.addInhousePart(validName, validPrice, STOC, MIN, MAX, MASINA_ID);
+            assert service.getAllParts().size() == 1;
+        }
+
+        @Test
+        @Tag("BVA")
+        @Tag("inhousePart")
+        @Tag("price")
+        @Timeout(2)
+        void addInhousePartValid_TC12_BVA() {
+            String validName = "P";
+            double validPrice = Double.MAX_VALUE + 0.01;
+            service.addInhousePart(validName, validPrice, STOC, MIN, MAX, MASINA_ID);
+            assert service.getAllParts().size() == 1;
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException interruptedException) {
+                interruptedException.printStackTrace();
+            }
         }
     }
 }
